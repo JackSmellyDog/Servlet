8d9f3cb662949cda2adc33efa2b3fed59b6848c1
andriy <dron.sh1@gmail.com>
2017-05-13 21:03:49 +0300

    null

--- src/main/java/servlets/AddAuthor.java
+++ src/main/java/servlets/AddAuthor.java
@@ -19,20 +19,32 @@ public class AddAuthor extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        long id = Long.parseLong(req.getParameter("id"));
-        String name = req.getParameter("fullName");
-        String phone = req.getParameter("phone");
-
         resp.setContentType("text/html");
-        AuthorDao dao = new AuthorSqlDao();
+
         try {
-            dao.insert(new Author(id, name, phone));
+            long id = Long.parseLong(req.getParameter("id"));
+            String name = req.getParameter("fullName");
+            String phone = req.getParameter("phone");
+
+            if (name  == null || name.isEmpty() || phone  == null || phone.isEmpty()){
+
+            }
+
+            AuthorDao dao = new AuthorSqlDao();
+            try {
+                dao.insert(new Author(id, name, phone));
+                PrintWriter writer = resp.getWriter();
+                writer.println("<h2>Author has been added</h2>");
+                writer.println("<br><a href = 'index.jsp'><< back</a>");
+                writer.close();
+            } catch (DataBaseException e) {
+                System.out.println("Terrible error!!!");
+            }
+        } catch (Exception e) {
             PrintWriter writer = resp.getWriter();
-            writer.println("<h2>Author has been added</h2>");
+            writer.println("<h2>Error author</h2>");
             writer.println("<br><a href = 'index.jsp'><< back</a>");
             writer.close();
-        } catch (DataBaseException e) {
-            System.out.println("Terrible error!!!");
         }
 
     }
