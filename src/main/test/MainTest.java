79056a8fe81ece26276fafc7ed444bd739d4ecdc
andriy <dron.sh1@gmail.com>
2017-05-14 14:02:11 +0300

    tests

---
+++ src/main/test/MainTest.java
@@ -0,0 +1,66 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void MySQLAuthorGetTest() throws Exception {
+
+    }
+
+    @Test
+    public void MySQLAuthorInsertTest() throws Exception {
+
+    }
+
+    @Test
+    public void MySQLAuthorUpdateTest() throws Exception {
+
+    }
+
+    @Test
+    public void MySQLAuthorDeleteTest() throws Exception {
+
+    }
+
+    @Test
+    public void MySQLPatentGetTest() throws Exception {
+
+    }
+
+    @Test
+    public void MySQLPatentInsertTest() throws Exception {
+
+    }
+
+    @Test
+    public void MySQLPatentUpdateTest() throws Exception {
+
+    }
+
+    @Test
+    public void MySQLPatentDeleteTest() throws Exception {
+
+    }
+
+    @Test
+    public void MongoGetTest() throws Exception {
+
+    }
+
+    @Test
+    public void MongoInsertTest() throws Exception {
+
+    }
+
+    @Test
+    public void MongoUpdateTest() throws Exception {
+
+    }
+
+    @Test
+    public void MongoDeleteTest() throws Exception {
+
+    }
+}
