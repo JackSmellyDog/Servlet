32eb94bb0433380af717c8a1bb3172f4e661dc12
andriy <dron.sh1@gmail.com>
2017-05-14 22:23:05 +0300

    tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,38 +12,6 @@ import java.util.Calendar;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException, DataBaseException {
-        AuthorDao dao = new AuthorSqlDao();
-        dao.getAll().forEach(System.out::println);
-    /*    System.out.println();
-        System.out.println(dao.getById(2));
-        Author deleted = dao.getById(4);
-        dao.deleteById(4);
-        dao.insert(deleted);
-        dao.update(5, new Author(5, "newName", "103"));
-
-        System.out.println();
-        System.out.println();
-        System.out.println();
-
-        PatentDao patentDao = new PatentSqlDao();
-        patentDao.getAll().forEach(System.out::println);
-        System.out.println();
-        System.out.println(patentDao.getById(2));
-        Patent deletedPatent = patentDao.getById(4);
-        patentDao.deleteById(4);
-        patentDao.insert(deletedPatent);
-        patentDao.update(5, new Patent(5, "newName", deleted, "lsfjl", "slfkj", Calendar.getInstance()));
-*/
-
-        //AuthorMongoDao mongoDao = new AuthorMongoDao();
-
-        /*mongoDao.insert(new Author(5, "testUpdate", "55555"));
-        System.out.println(mongoDao.getById(5));
-
-        mongoDao.update(5, new Author(5, "victory", "444444"));
-        System.out.println(mongoDao.getById(5));*/
-
-        //mongoDao.getAll().forEach(System.out::println);
 
     }
 }
--- src/main/test/MainTest.java
+++ src/main/test/MainTest.java
@@ -1,5 +1,6 @@
 import dao.AuthorDao;
 import dao.PatentDao;
+import dao.impl.AuthorMongoDao;
 import dao.impl.AuthorSqlDao;
 import dao.impl.PatentSqlDao;
 import model.Author;
@@ -110,21 +111,46 @@ public class MainTest {
 
     @Test
     public void MongoGetTest() throws Exception {
+        AuthorDao authorDao = new AuthorMongoDao();
+        Author author = new Author(1, "Ivanov Ivan Ivanovich", "11111111111");
+        Assert.assertEquals(author, authorDao.getById(1));
 
     }
 
     @Test
     public void MongoInsertTest() throws Exception {
+        AuthorDao authorDao = new AuthorMongoDao();
+        Author author = new Author(99, "Test User", "999");
+        authorDao.insert(author);
 
+        Assert.assertEquals(author, authorDao.getById(99));
+
+        authorDao.deleteById(99);
     }
 
     @Test
     public void MongoUpdateTest() throws Exception {
+        AuthorDao authorDao = new AuthorMongoDao();
+        Author author = new Author(99, "Test User", "999");
+        authorDao.insert(author);
 
+        Author newAuthor = new Author(99, "New Test User", "111");
+        authorDao.update(99, newAuthor);
+
+        Assert.assertEquals(newAuthor, authorDao.getById(99));
+        Assert.assertNotEquals(author, authorDao.getById(99));
+
+        authorDao.deleteById(99);
     }
 
     @Test
     public void MongoDeleteTest() throws Exception {
+        AuthorDao authorDao = new AuthorMongoDao();
+        Author author = new Author(99, "Test User", "999");
+        authorDao.insert(author);
 
+        Assert.assertTrue(authorDao.getAll().contains(author));
+        authorDao.deleteById(99);
+        Assert.assertFalse(authorDao.getAll().contains(author));
     }
 }
